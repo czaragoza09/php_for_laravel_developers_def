ae4f6b2c10c6f566af3fc0b4000efae2a8faed59
czaragoza09 <czaragoza@iesebre.com>
2023-06-08 20:21:01 +0200

    Video 110 acabat

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('tasks');
+    $task1 = new stdClass();
+    $task1->name="Task 1";
+    $task1->completed = 1;
+    $task2 = new stdClass();
+    $task2->name="Task 2";
+    $task2->completed = 1;
+
+    $tasks = [
+        $task1,
+        $task2
+    ];
+    return view('tasks' , [
+        'tasks' => $tasks
+        ]);
 });
 
 Route::get('/tasks', function () {
